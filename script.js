a7ac0cfc7df723aa2a9c18e245eb45254553b532
Laassel <lassel.giovanni@gmail.com>
2024-11-05 16:18:29 +0100

    Add loop and api request

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello World');
+
+const emailListElement = document.getElementById('email-list');
+// console.log(emailListElement);
+
+// Creo un ciclo for per iterare 10 volte e chiedere 10 email all'api
+for(let i = 0; i < 10; i++) {
+    // console.log(i)
+
+    // Creo una richiesta get da fare all'api usando axios
+    axios
+    .get(' https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {
+        console.log(response);
+    })
+
+}
